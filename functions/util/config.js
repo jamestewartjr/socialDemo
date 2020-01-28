ad7565aa910f624361cc71972a07e0b84b60f40b
James Stewart <jamestewartjr1@gmail.com>
2020-01-28 11:56:00 -0500

    API routes complete

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAt9i5XiZyom2mXsOSpLfd_0ILgDZ6uLhM",
+  authDomain: "socialdemospa.firebaseapp.com",
+  databaseURL: "https://socialdemospa.firebaseio.com",
+  projectId: "socialdemospa",
+  storageBucket: "socialdemospa.appspot.com",
+  messagingSenderId: "456078504994",
+  appId: "1:456078504994:web:236a86e05020e2b717867b",
+  measurementId: "G-17M8KBTB0B"
+};
