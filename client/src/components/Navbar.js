365bf84b252f52436a6c3c1c9af0f3b0c058f8d0
James Stewart <jamestewartjr1@gmail.com>
2020-02-19 19:06:36 -0500

    Add working navigation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
+import Link from 'react-router-dom/Link';
 
 class Navbar extends Component { 
   render() {
     return (
       <AppBar>
         <Toolbar>
-          <Button color="inherit"> Home </Button>
-          <Button color="inherit"> Signup </Button>
-          <Button color="inherit"> Login </Button>
-
+          <Button color="inherit" component={Link} to="/"> Home </Button>
+          <Button color="inherit" component={Link} to="/signup"> Signup </Button>
+          <Button color="inherit" component={Link} to="/login"> Login </Button>
         </Toolbar>
       </AppBar>
     )
