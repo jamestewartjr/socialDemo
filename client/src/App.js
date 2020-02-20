3ceb394b203f1b4359631ef5fa6b0d9d55da282f
James Stewart <jamestewartjr1@gmail.com>
2020-02-19 23:59:51 -0500

    Update theme, home page and App routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Home, Login, Signup} from './pages/index.js';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6ec6ff',
+      main: '#2196f3',
+      dark: '#0069c0',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#ff6659',
+      main: '#d32f2f',
+      dark: '#9a0007',
+      contrastText: '#fff',
+    },
+  },
+  typography: { userNextVariants: true}
+})
 class App extends Component {
   render() {
-    return ( 
-      <div className="App"> 
-      <Router>
-        <Navbar/>
-        <div className='container'>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-          </Switch>
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App"> 
+          <Router>
+            <Navbar/>
+            <div className='container'>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+              </Switch>
+            </div>
+          </Router>
         </div>
-      </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      </div>
+      </MuiThemeProvider>
+
     )
   }
 }
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import '../App.css'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
 class Home extends Component {
+  state = {
+    posts: null
+  }
+  
+  componentDidMount(){
+    axios.get('/posts')
+      .then( response => {
+        console.log(response)
+        this.setState({
+          posts: response.data
+        })
+      })
+      .catch( error => console.error(error))
+  }
+
   render() {
+    let recentPosts = this.state.posts 
+      ? (this.state.posts.map(posts => 
+          <p>{posts.body}</p>
+        )) 
+      : <p>Loading.... </p>
     return ( 
-      <div> 
-        <h1>Home</h1>
-      </div>
+      <Grid container spacing={2}>
+
+        <Grid item sm={8} xs={12}>
+          {recentPosts}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>More</p>
+        </Grid>
+      </Grid>
     )
   }
 }
